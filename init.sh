451beb54edceee807aaee2e447616e5cca1a2eb4
Tarrex <tarrex@users.noreply.github.com>
2018-04-24 15:32:16 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -63,6 +63,7 @@ if [ -f "~/.vimrc" ]; then
     mv ~/.vimrc /tmp/.vimrc 
 fi 
 cp .vimrc ~/.vimrc
-git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +q! +q!
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +q! +q!
 echo "vim has been initialized."
