4e2bfddf0dbbd816518b11b7b94cb6357965d9b4
tarrex <tarrexchen@gmail.com>
2021-05-29 17:51:03 +0800

    shell: update ruby bundler directory location

--- .config/init.sh
+++ .config/init.sh
@@ -617,8 +617,13 @@ export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME/jupyter"
 export PYLINTHOME="$XDG_CACHE_HOME/pylint"
 
 # Ruby
+# gems
 export GEM_HOME="$XDG_DATA_HOME/gem"
 export GEM_SPEC_CACHE="$XDG_CACHE_HOME/gem"
+# bundler
+export BUNDLE_USER_CONFIG="$XDG_CONFIG_HOME/bundle"
+export BUNDLE_USER_CACHE="$XDG_CACHE_HOME/bundle"
+export BUNDLE_USER_PLUGIN="$XDG_DATA_HOME/bundle"
 
 # Java
 if command -v java &> /dev/null; then
