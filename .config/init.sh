5b1849de3c95138ce0d96d17b0582606c4036bf3
tarrex <tarrexchen@gmail.com>
2022-07-02 14:27:14 +0800

    shell: add xbin support

--- .config/init.sh
+++ .config/init.sh
@@ -858,6 +858,17 @@ websearch() {
     fi
 }
 
+# xbin - https://xbin.io
+function xbin() {
+    command="$1"
+    args="${*:2}"
+    if [ -t 0 ]; then
+        curl -sS -X POST "https://xbin.io/${command}" -H "X-Args: ${args}"
+    else
+        curl -sS --data-binary @- "https://xbin.io/${command}" -H "X-Args: ${args}"
+    fi
+}
+
 # ============> Finally <============
 # Remove duplicate path
 if [[ -n $PATH ]]; then
